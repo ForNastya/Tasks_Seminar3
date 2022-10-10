3c97fe6e7338394207f55690a5f25c1271d979ec
Anastasiya <moya_pochta_n.k@mail.ru>
2022-10-10 15:42:46 +0300

    Task 01

---
+++ Task_01/Program.cs
@@ -0,0 +1,6 @@
+﻿//Найти кубы чисел от 1 до N
+Console.WriteLine("Введите число ");
+number = int.Parse(Console.ReadLine());
+for (int i =1; i<= number; i++)
+Console.WriteLine($"{Match.Pow(i, 3)}, ");
+Console.WriteLine(" ");
