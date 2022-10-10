7fb4c97e63fc4b4edba40f4a52ed0906c4b63e15
Anastasiya <moya_pochta_n.k@mail.ru>
2022-10-10 15:43:22 +0300

    Task 02

---
+++ Task_02/Program.cs
@@ -0,0 +1,8 @@
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine("Введите любое число");
+int number = int.Parse(Console.ReadLine());
+int a = number / 100;
+int b = number / 10 % 10;
+int c = number % 10;
+int sum = a + b + c;
+Console.WriteLine("Сумма цифр числв = " + sum);
